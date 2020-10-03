61618c4d0d2c1c4b37d1693e2a045b143efda2c5
John Tucker <john@larkintuckerllc.com>
2020-10-03 17:21:33 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    author="John Tucker",
+    author_email="john@larkintuckerllc.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    description="A small example package",
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    name="example-prj-sckmkny",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.8',
+    url="https://github.com/larkintuckerllc/example-prj-sckmkny",
+    version="0.1.1",
+)
+
