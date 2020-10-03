0aa346b28c3e4303d15c14f3473fc22afcde7fb1
John Tucker <john@larkintuckerllc.com>
2020-10-03 18:46:23 -0400

    first stab dep

--- setup.py
+++ setup.py
@@ -3,6 +3,13 @@ import setuptools
 with open("README.md", "r") as f:
     long_description = f.read()
 
+install_requires = []
+with open('requirements.txt') as f:
+    for line in f:
+        line, _, _ = line.partition('#')
+        line = line.strip()
+        install_requires.append(line)
+
 setuptools.setup(
     author="John Tucker",
     author_email="john@larkintuckerllc.com",
@@ -12,6 +19,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     description="A small example project with single package.",
+    install_requires=install_requires,
     license='MIT',
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -21,4 +29,3 @@ setuptools.setup(
     url="https://github.com/larkintuckerllc/example-prj-sckmkny",
     version="0.2.0",
 )
-
