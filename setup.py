ffb435eb17afd8469ef8237628e6cf8c4c4091bc
John Tucker <john@larkintuckerllc.com>
2020-10-03 20:38:53 -0400

    man

--- setup.py
+++ setup.py
@@ -10,6 +10,22 @@ with open("README.md", "r") as f:
 #         line = line.strip()
 #         install_requires.append(line)
 
+# install_requires = [
+#     'certifi',
+#     'chardet',
+#     'idna',
+#     'requests',
+#     'urllib3',
+# ]
+
+install_requires = [
+    'certifi==2020.6.20',
+    'chardet==3.0.4',
+    'idna==2.10',
+    'requests==2.24.0',
+    'urllib3==1.25.10',
+]
+
 setuptools.setup(
     author="John Tucker",
     author_email="john@larkintuckerllc.com",
@@ -19,8 +35,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     description="A small example project with single package.",
-    # install_requires=install_requires,
-    install_requires=['requests'],
+    install_requires=install_requires,
     license='MIT',
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -28,5 +43,5 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     python_requires='>=3.8',
     url="https://github.com/larkintuckerllc/example-prj-sckmkny",
-    version="0.2.1",
+    version="0.2.3",
 )
